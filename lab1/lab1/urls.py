ecba6dec58efc556f7c096557b54661cfcc28bd9
ST3410 <ST3410@TE.local>
2024-12-04 17:01:27 +0600

    {первая лаба}

---
+++ lab1/lab1/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('about/', views.about),
+    path('', views.index),
+    path('posts/', include('posts.urls')),
+    path('communities/', include('communities.urls'))
+    
+]
