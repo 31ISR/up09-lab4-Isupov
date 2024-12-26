35cfe22414ded5805a9ef57bee6e95a2adcff694
nsssssss <zzz.zz.z.zz1232176@gmail.com>
2024-12-26 06:59:45 +0600

    Готовоемае

---
+++ lab1/communities/forms.py
@@ -0,0 +1,6 @@
+from django import forms 
+from . import models
+class CreateCommunitie(forms.ModelForm):
+    class Meta:
+        model = models.Communities
+        fields = ['title', 'description', 'slug', 'banner']
--- lab1/communities/views.py
+++ lab1/communities/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Communities
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from . import forms 
 
 def communities(request):
     communities = Communities.objects.all().order_by('-date')
@@ -11,7 +13,14 @@ def communities_page(request, slug):
     return render(request, 'communities/communities_page.html', {'communitie': communitie})
 
 
-
 @login_required(login_url="/users/login/")
 def communities_new(request):
-    return render(request, 'communities/communities_new.html')
+    if request.method == 'POST': 
+        form = forms.CreateCommunitie(request.POST, request.FILES) 
+        if form.is_valid():
+            newpost = form.save(commit=False) 
+            newpost.save()
+            return redirect('communities:list')
+    else:
+        form = forms.CreateCommunitie()
+    return render(request, 'communities/communities_new.html', { 'form': form })
